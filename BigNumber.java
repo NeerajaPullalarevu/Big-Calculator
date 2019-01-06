332d79bb26daab8902ad45fe8f1a64aac5161400
Shubham Kokul <shubhamkokul@gmail.com>
2019-01-06 12:39:31 -0500

    Add files via upload
    Following the Java FXML structure to create a Big decimal calculator using Linked List.

---
+++ BigNumber.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatorbig;
+
+/**
+ *
+ * @author aditya
+ */
+
+public class BigNumber {
+    private SLLNode first;
+    int decimalPointPosition;
+    public BigNumber(String s){
+        
+        //list1.printFirstToLast();
+    }
+   
+   
+}
